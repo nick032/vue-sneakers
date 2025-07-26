3aff350d01cf84f3aaa8df1ab3e873967704bd8c
nick032 <nick.x.a.k@gmail.com>
2025-07-26 10:15:07 +0300

    Начало проекта

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,29 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header.vue";
+import "./assets/main.css";
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <div class="w-4/5 m-auto bg-white h-screen rounded-xl shadow-xl mt-14">
+    <Header />
+
+    <div class="">
+      <div
+        class="relative m-10 border shadow-md border-slate-100 rounded-4xl p-8 cursor-pointer hover:-translate-y-2 duration-200 ease-in"
+      >
+        <img class="absolute top-8 left-8" src="/like-1.svg" alt="Like" />
+        <img src="/sneakers/sneakers-1.jpg" alt="Sneakers" />
+        <p>Мужские кроссовки Nike Blazer Mid Suede</p>
+        <div class="flex justify-between">
+          <div class="flex flex-col">
+            <span class="text-slate-400">Цена:</span>
+            <b>12999 руб.</b>
+          </div>
+          <img src="/plus.svg" alt="Plus" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header
+    class="flex justify-between items-center px-10 py-8 border-b border-slate-300"
+  >
+    <div class="flex gap-4">
+      <img src="/logo.png" alt="Logo" class="w-10" />
+      <div>
+        <h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
+        <p class="text-slate-400">Магазин лучших кроссовок</p>
+      </div>
+    </div>
+
+    <ul class="flex gap-10">
+      <li
+        class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black duration-200 ease-in"
+      >
+        <img src="/cart.svg" alt="Cart" />
+        <b>1205 руб.</b>
+      </li>
+      <li
+        class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black duration-200 ease-in"
+      >
+        <img src="/heart.svg" alt="Cart" />
+        <span>Закладки</span>
+      </li>
+      <li
+        class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black duration-200 ease-in"
+      >
+        <img src="/profile.svg" alt="Cart" />
+        <span>Профиль</span>
+      </li>
+    </ul>
+  </header>
+</template>
